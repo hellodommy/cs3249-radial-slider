70a87bb47640f1c90e6e7fbb71b1feba4838982a
hellodommy <dominique@u.nus.edu>
2021-03-08 22:59:49 +0800

    Redo state chart

--- statechart.js
+++ statechart.js
@@ -1,61 +1,83 @@
-const updateTemp = assign({
-	currTemp: (context, event) => context.currTemp === context.newCurrTemp,
-	targetTemp: (context, event) => context.targetTemp == context.newTarTemp
+// Available variables:
+// - Machine
+// - interpret
+// - assign
+// - send
+// - sendParent
+// - spawn
+// - raise
+// - actions
+// - XState (all XState exports)
+
+import { assign } from "xstate/lib/actionTypes";
+
+const updateTargetTemp = assign({
+  targetTemp: (context, event) => event.targetTemp
 })
 
-const thermostateMachine = Machine(
+const updateCurrentTemp = assign({
+  currTemp: (context, event) => event.currTemp
+})
+
+export const thermostatMachine = Machine(
   {
     id: "thermostat",
     initial: "off",
     context: {
-      currTemp: 72,
-      newCurrTemp: 72,
       targetTemp: 72,
-      newTarTemp: 72,
+      currTemp: 72,
       dT: 2,
       dTCool: 1.5,
       dTHeat: 1.5,
+      colour: "#E5E7EB",
     },
     states: {
       off: {
         on: {
-          "": [
-            {
-              target: "tempChecking",
-              cond: "didTempChange",
-              action: updateTemp,
-            },
-          ],
+          entry: [assign({ colour: "#E5E7EB" })],
+          CURR_TEMP_CHANGE: {
+            target: "tempCheck",
+            actions: [updateCurrentTemp],
+          },
+          TARGET_TEMP_CHANGE: {
+            target: "tempCheck",
+            actions: [updateTargetTemp],
+          },
         },
       },
-      tempChecking: {
+      cooling: {
+        on: {
+          entry: [assign({ colour: "#BFDBFE" })],
+          CURR_TEMP_CHANGE: {
+            target: "tempCheck",
+            actions: [updateCurrentTemp],
+          },
+          TARGET_TEMP_CHANGE: {
+            target: "tempCheck",
+            actions: [updateTargetTemp],
+          },
+        },
+      },
+      heating: {
+        on: {
+          entry: [assign({ colour: "#FECACA" })],
+          CURR_TEMP_CHANGE: {
+            target: "tempCheck",
+            actions: [updateCurrentTemp],
+          },
+          TARGET_TEMP_CHANGE: {
+            target: "tempCheck",
+            actions: [updateTargetTemp],
+          },
+        },
+      },
+      // transient state
+      tempCheck: {
         on: {
           "": [
             { target: "off", cond: "isNormal" },
             { target: "cooling", cond: "isHot" },
             { target: "heating", cond: "isCold" },
-          ],
-        },
-      },
-      cooling: {
-        on: {
-          "": [
-            {
-              target: "tempChecking",
-              cond: "didTempChange",
-              action: updateTemp,
-            },
-          ],
-        },
-      },
-      heating: {
-        on: {
-          "": [
-            {
-              target: "tempChecking",
-              cond: "didTempChange",
-              action: updateTemp,
-            },
           ],
         },
       },
@@ -77,12 +99,6 @@ const thermostateMachine = Machine(
         return (
           context.currTemp < context.targetTemp + context.dT + context.dTCool &&
           context.currTemp > context.targetTemp - context.dT - context.dTHeat
-        );
-      },
-      didTempChange: (context, event) => {
-        return (
-          context.currTemp !== context.newCurrTemp ||
-          context.targetTemp !== context.newTarTemp
         );
       },
     },
